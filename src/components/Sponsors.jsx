1777aa599eeac86d75b4fc9ef702a432bcb68f2e
Anirudh <anirudhx5@gmail.com>
2017-09-01 13:32:45 +0100

    sponsor images

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -7,31 +7,29 @@ let sponsors = [
     "imageLink": "https://pbs.twimg.com/profile_images/3375409673/54825068b089a0dd74570723e8c054aa.png"
   },
   {
-    "title": "UCL ISD",
-    "imageLink": "https://pbs.twimg.com/profile_images/3375409673/54825068b089a0dd74570723e8c054aa.png"
+    "title": "Github",
+    "imageLink": "https://www.pmg.com/content/uploads/2016/12/github-logo.png"
   },
   {
-    "title": "UCL ISD",
-    "imageLink": "https://pbs.twimg.com/profile_images/3375409673/54825068b089a0dd74570723e8c054aa.png"
+    "title": "Google",
+    "imageLink": "http://diylogodesigns.com/blog/wp-content/uploads/2016/04/new-google-logo-png.png"
   },
   {
-    "title": "UCL ISD",
-    "imageLink": "https://pbs.twimg.com/profile_images/3375409673/54825068b089a0dd74570723e8c054aa.png"
-  }
+    "title": "Facebook",
+    "imageLink": "http://www.freeiconspng.com/uploads/facebook-announces-clickable-hashtags--resolution-media-17.png"
+  },
 ]
 
 
 const Sponsor = (s, key) => (
-    <div className="card-panel" key={key}>
-      <img src={s.imageLink} className="responsive-img" />
-    </div>
+    <img key={key} src={s.imageLink} />
 )
 
 export default class Sponsors extends React.Component {
 
     render () {
       return (
-        <div className="container">
+        <div className="">
           <h1>Sponsors</h1>
           <div className="sponsors">
             {sponsors.map((s, i) => Sponsor(s, i))}
---
+++ src/components/Venue.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default class Venue extends React.Component {
+
+    render () {
+      return (
+        <div className="venue container">
+          <h1>The Venue</h1>
+        </div>
+      )
+    }
+
+}
