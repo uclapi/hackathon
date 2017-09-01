15bf5f8e4d1dad929ae2971c1378e3cf45a3a5b3
Anirudh <anirudhx5@gmail.com>
2017-09-01 11:08:57 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import logoFile from './../img/simpleAPILogoWhite.svg';
+
 
 export default class Header extends React.Component {
 
     render () {
       return (
         <div className="header">
-
+          <img src={logoFile} />
+          <h1>UCL API Hackathon</h1>
+          <button className="btn">Register Now</button>
         </div>
       )
     }
