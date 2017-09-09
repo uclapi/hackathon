675949e6df7f5f67383f51d26394eed10d7d192a
Wilhelm Klopp <wil.klopp@gmail.com>
2017-09-09 22:43:02 +0200

    Small improvements

--- backend/applications/views.py
+++ backend/applications/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render, HttpResponse, redirect
-from django.http import JsonResponse
-from django.core.serializers.json import DjangoJSONEncoder
-from django.views.decorators.csrf import csrf_protect
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
 import requests
 
 from .helpers import generate_state, generate_access_code
 
 import os
 import json
-import requests
 
 
+@ensure_csrf_cookie
 def render_home(request):
     return render(request, 'home.html', {
         'initial_data': {
@@ -24,7 +22,7 @@ def process_login(request):
     state = generate_state()
     request.session["state"] = state
     auth_url = os.environ.get("UCLAPI_URL") + "/oauth/authorise"
-    auth_url += "?client_id=" + os.environ.get("UCLAPI_CLIENT_ID")
+    auth_url += "?client_id=" + os.environ["UCLAPI_CLIENT_ID"]
     auth_url += "&state=" + state
 
     return redirect(auth_url)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jgewf^dmv3rq$o8pvg8t*-_mf2e#pfbl-rnj3atqd9p+78k2zv'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DJANGO_DEBUG'] == 'True'
 
 ALLOWED_HOSTS = []
 
