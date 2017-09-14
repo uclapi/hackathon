627ffd538920ffe8e4b0b0b382f82baa46218187
Wilhelm Klopp <wil.klopp@gmail.com>
2017-09-14 10:06:21 +0200

    Add GitHub 'Fork Me' ribbon

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import GitHubForkRibbon from 'react-github-fork-ribbon';
+
 import Header from './Header.jsx';
 import Description from './Description.jsx';
 import Venue from './Venue.jsx';
@@ -15,6 +17,13 @@ export default class HomePage extends React.Component {
     render () {
       return (
         <div>
+          <GitHubForkRibbon
+            href="https://github.com/uclapi/hackathon"
+            target="_blank"
+            position="right"
+          >
+            Fork me on GitHub
+          </GitHubForkRibbon>
           <Header />
           <Description />
           <Venue />
