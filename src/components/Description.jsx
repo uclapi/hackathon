3a0586483da8b6f9cce58b6fa6052c1e2816ce7d
Wilhelm Klopp <wil.klopp@gmail.com>
2017-09-14 10:27:41 +0200

    More copy improvements

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -12,13 +12,13 @@ export default class Description extends React.Component {
               UCL API Hackathon is a 24-hour event held at UCL. It's an opportunity for you to spend a weekend building on top of the student-developed <a href="https://medium.freecodecamp.org/what-is-an-api-in-english-please-b880a3214a82" target="_blank">API</a> for UCL.
             </p>
             <p>
-              The API has been built to increase access and usability of UCL services, enabling the development of an ecosystem of student-made tools and applications. Students can now build tools which they themselves will use and maintain!
+              The API has been built to create a simple programmatic interface to UCL’s digital services, enabling the development of an ecosystem of student-made tools and applications. Students can now build tools which they themselves will use and maintain!
             </p>
             <p>
-              If you want to use the open API to build tools which will help yourself and other students, then this event is definitely for you! Or, if you want to learn about APIs, the technologies used, and learn about other new tech, then come along and attend our workshops and mini-events.
+              If you want to use the API to build tools that help yourself and other students, then this event is for you! Or, if you want to learn more about APIs, programming, and new technologies, then come along and dive right in!
             </p>
             <p>
-              At the end of the event, you can show off what you've built and learned by presenting to everyone who attended at the end.
+              At the end of the event, you can show off what you've built and learned by presenting to everyone who attended!
             </p>
           </div>
         </div>
