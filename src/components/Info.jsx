09004ea8291164bee22f297f7014edb4596675dc
emilyruby <zcabeme@ucl.ac.uk>
2017-10-06 21:51:01 +0100

    small needless change :)

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -10,7 +10,7 @@ export default class Info extends React.Component {
             <h1>What is UCL API?</h1>
             <p>UCL API is a <b>student-built</b> platform for <b>student developers</b> to improve the <b>student experience</b> of everyone at UCL.</p>
             <p>
-              Its goal is to provide a simple, well documented, and comprehensive programmatic interface to UCL’s digital services. Anyone at UCL can build on top of this API to build apps, websites, tools, or simply to scratch their own itch.
+              Its goal is to provide a simple, well-documented, and comprehensive programmatic interface to UCL’s digital services. Anyone at UCL can build on top of this API to build apps, websites, tools, or simply to scratch their own itch.
             </p>
             <p>
               All of the information you need to get started can be found at <a href="https://uclapi.com">uclapi.com</a>, where you can see projects built using the API, as well as examples about how to use the API. Documentation is located at <a href="https://docs.uclapi.com">docs.uclapi.com</a>.
