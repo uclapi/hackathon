5896b8c7ae153e8fcb3df893e5b5e874c701842b
TuxedoFish <harryliversedge@gmail.com>
2019-11-15 11:57:25 +0000

    Added waypoints for animations

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,6 +9,7 @@ import 'Styles/navbar.scss';
 
 // Images
 import glitch from 'Images/glitch.png';
+import confetti from 'Images/confetti.png';
 
 // Components
 import { Row, Column, TextView, ButtonView, CardView, ImageView, NavBar, Footer } from 'Layout/Items.jsx';
@@ -57,6 +58,11 @@ const LeftSlideIn = posed.div({
   closed: { 'marginLeft': '-200px' }
 });
 
+const FallDown = posed.div({
+  open: { 'opacity' : '0' },
+  closed: { 'opacity' : '1' }
+});
+
 export default class HomePage extends React.Component {
     constructor(props) {
       super(props);
@@ -68,12 +74,13 @@ export default class HomePage extends React.Component {
             "landingpage" : false,
             "description" : false,
             "categories" : false,
+            "2018image" : false,
         }
       }
 
       this.toggleAnimation = this.toggleAnimation.bind(this);
     }
-    
+
     toggleAnimation(name, isOn) {
       if(this.DEBUGGING) { console.log("animation triggered for: " + name); }
 
@@ -105,7 +112,6 @@ export default class HomePage extends React.Component {
 
           <Waypoint
             onEnter={ (props) => {this.toggleAnimation("landingpage", true)} }
-            onLeave={ (props) => {this.toggleAnimation("landingpage", false)} }
           />
 
           <Row height = '600px' styling='splash-parallax'>
@@ -121,7 +127,6 @@ export default class HomePage extends React.Component {
 
           <Waypoint
             onEnter={ (props) => {this.toggleAnimation("description", true)} }
-            onLeave={ (props) => {this.toggleAnimation("description", false)} }
           />
 
           <Row styling="primary">
@@ -144,7 +149,6 @@ export default class HomePage extends React.Component {
 
           <Waypoint
             onEnter={ (props) => {this.toggleAnimation("categories", true)} }
-            onLeave={ (props) => {this.toggleAnimation("categories", false)} }
           />
 
           <Row styling='splash-parallax'>
@@ -163,6 +167,45 @@ export default class HomePage extends React.Component {
                   </FocusIn>
                 )
               }
+            </Column>
+          </Row>
+
+          <Waypoint
+            onEnter={ (props) => {this.toggleAnimation("2018description", true)} }
+          />
+
+          <Row styling="primary">
+            <Column width='4-10' horizontalAlignment='center'>
+              <TextView text="What did the previous contestants make?" heading={1} align={'center'}/>
+              <TextView text={`UCL API Hackathon is a 24-hour event held at UCL. It's an 
+                  opportunity for you to spend a weekend building on top of the student-developed
+                  API for UCL.`} heading={5} align={'left'}/>
+              <TextView text={`The API has been built to create a simple programmatic interface 
+              to UCL’s digital services, enabling the development of an ecosystem of student-made
+                tools and applications. Students can now build tools which they themselves will 
+                use and maintain!`} heading={5} align={'left'}/>
+              <TextView text={`If you want to use the API to build tools that help yourself and 
+                other students, then this event is for you! Or, if you want to learn more about 
+                APIs, programming, and new technologies, then come along and dive right in!`} heading={5} align={'left'}/>
+              <TextView text={`At the end of the event, you can show off what you've built 
+                and learned by presenting to everyone who attended!`} heading={5} align={'left'}/>
+            </Column>
+          </Row>
+
+          <Waypoint
+            onEnter={ (props) => {this.toggleAnimation("2018image", true)} }
+          />
+
+          <Row height='400px' styling='team-parallax'>
+
+            <FallDown pose={animations["2018image"] ? 'open' : 'closed'} style={{'transitionDuration' :'1000'}}>
+              <ImageView src={confetti} width="100%" height="400px" description="confetti from a party"/>
+            </FallDown>
+
+            <Column width='1-1' horizontalAlignment='center' verticalAlignment='center'>
+              <LeftSlideIn pose={animations["2018image"] ? 'open' : 'closed'} style={{'transitionTimingFunction' :'cubic-bezier(0.175, 0.885, 0.32, 1.275)'}}>
+                <TextView text="The 2018 Hackathon Class!" heading={1} align={'center'}/>
+              </LeftSlideIn>
             </Column>
           </Row>
 
