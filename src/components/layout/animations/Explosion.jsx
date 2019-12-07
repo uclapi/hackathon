b0dc6a07437b22beefcda00fc1d13a19d8bb48cc
TuxedoFish <harryliversedge@gmail.com>
2019-12-07 17:17:11 +0000

    Made it possible to replay the animation using a click

--- src/components/layout/animations/Explosion.jsx
+++ src/components/layout/animations/Explosion.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 /**
 REQUIRED ATTRIBUTES:
@@ -16,38 +16,28 @@ this.props.gravity
 export default class Explosion extends React.Component {
 
 	constructor(props) {
-		super(props);
+		super(props)
 
 		// Constants for the class
-		this.DEBUGGING = false;
-		this.DELTA_TIME = 16;
-		this.RESISTENCE = 50;
-		this.MAX_TIME = 2500;
-		this.FADE_TIME = 500;
+		this.DEBUGGING = false
+		this.DELTA_TIME = 16
+		this.RESISTENCE = 50
+		this.MAX_TIME = 2500
+		this.FADE_TIME = 500
 
 		// Bind functions
-		this.updateCanvas = this.updateCanvas.bind(this);
-
-		var radius = [];
-		var theta = [];
-		var delay = [];
-		var size = [];
-
-		// Create the particles
-		for(var i=0; i<this.props.particles; i++) {
-			radius.push(0);
-			theta.push(Math.random()*Math.PI*2);
-			delay.push(Math.random()*700);
-			size.push( ( this.props.maxsize / 2 )+ ( Math.random()*(this.props.maxsize / 2) ) );
-		}
+		this.updateCanvas = this.updateCanvas.bind(this)
+		this.safeStopAnimation = this.safeStopAnimation.bind(this)
+		this.setupAnimation = this.setupAnimation.bind(this)
+		this.startAnimation = this.startAnimation.bind(this)
 
 		// Save class and stylings to the state
 		this.state = {
 			speed: this.props.speed,
-			size: size,
-			delay: delay,
-			radius: radius,
-			theta: theta,
+			size: [],
+			delay: [],
+			radius: [],
+			theta: [],
 			colors: ["#a864fd", "#29cdff", "78ff44", "ff718d", "#fdff6a",
 			"#f6db5f", "ffb554", "fe5e51", "9e3d64", "36abb5"],
 			intervalId: 0,
@@ -56,23 +46,65 @@ export default class Explosion extends React.Component {
 			verticalDisplacement: 0,
 			width: 100,
 			height: 100,
-		};
+			active: false,
+		}
 
+	}
+
+	setupAnimation() {
+		var radius = []
+		var theta = []
+		var delay = []
+		var size = []
+
+		// Create the particles
+		for(var i=0; i<this.props.particles; i++) {
+			radius.push(0)
+			theta.push(Math.random()*Math.PI*2)
+			delay.push(Math.random()*700)
+			size.push( ( this.props.maxsize / 2 )+ ( Math.random()*(this.props.maxsize / 2) ) )
+		}
+
+		this.setState({
+			size: size,
+			delay: delay,
+			radius: radius,
+			theta: theta,
+			totalTime: 0,
+			verticalSpeed: 0, 
+			verticalDisplacement: 0,
+		})
 	}
 
 	startAnimation() {
+		this.safeStopAnimation()
 
+		setTimeout(() => {
+			this.setupAnimation()
+
+			var intervalId = setInterval(this.updateCanvas, 16)
+			this.setState({intervalId: intervalId, active: true})
+		}, 100)
+	}
+
+	safeStopAnimation() {
+		// Only delete if active i.e not destroyed yet
+		if(this.state.active) {
+			clearInterval(this.state.intervalId)
+			// Update state to prevent duplicate deletions
+			this.setState({active: false})
+		}
 	}
 
 	componentDidMount() {
-		var intervalId = setInterval(this.updateCanvas, 16);
+		var intervalId = setInterval(this.updateCanvas, 16)
 		// store intervalId in the state so it can be accessed later:
-		this.setState({intervalId: intervalId});
+		this.setState({intervalId: intervalId, active: true})
 	}
 
 	componentWillUnmount() {
 		// use intervalId from the state to clear the interval
-		clearInterval(this.state.intervalId);
+		this.safeStopAnimation()
 	}
 
 	updateCanvas() {
@@ -93,59 +125,61 @@ export default class Explosion extends React.Component {
  
 	    	if(this.DEBUGGING) { console.log("w: " + width + ", h: " + height) }
 
-		    const context = this.refs.canvas.getContext('2d');
-		    context.clearRect(0,0, width, height);
+		    const context = this.refs.canvas.getContext('2d')
+		    context.clearRect(0,0, width, height)
 
 		    // Loop over colors
-		    var colorIndex = 0;
-		    const centerX = width / 2;
-		    const centerY = height / 2;
+		    var colorIndex = 0
+		    const centerX = width / 2
+		    const centerY = height / 2
 
-		    var { radius, delay, theta, totalTime, size, colors, verticalDisplacement} = this.state;
+		    var { radius, delay, theta, totalTime, size, colors, verticalDisplacement} = this.state
 
-		    const timeLeft = this.MAX_TIME - totalTime;
+		    const timeLeft = this.MAX_TIME - totalTime
 		    if( timeLeft < this.FADE_TIME) {
-		    	context.globalAlpha = timeLeft / this.FADE_TIME;
+		    	context.globalAlpha = timeLeft / this.FADE_TIME
+		    } else {
+		    	context.globalAlpha = 1
 		    }
 
 			// Create the particles
 			for(var i=0; i<this.props.particles; i++) {
 				if(delay[i] < totalTime) {
-					radius[i] =  radius[i] + (this.state.speed * 16 / 1000);
-					if(this.DEBUGGING) { console.log("polar coords, r: " + radius[i] + " theta: " + theta[i]); }
+					radius[i] =  radius[i] + (this.state.speed * 16 / 1000)
+					if(this.DEBUGGING) { console.log("polar coords, r: " + radius[i] + " theta: " + theta[i]) }
 
-					const x = centerX + (Math.cos(theta[i]) * radius[i]);
-					var y = centerY + (Math.sin(theta[i]) * radius[i]);
-					if(this.props.gravity) { y += verticalDisplacement; }
-					if(y > height) { y = height; }
+					const x = centerX + (Math.cos(theta[i]) * radius[i])
+					var y = centerY + (Math.sin(theta[i]) * radius[i])
+					if(this.props.gravity) { y += verticalDisplacement }
+					if(y > height) { y = height }
 
-				    context.fillStyle = colors[colorIndex];
-				    context.beginPath();
+				    context.fillStyle = colors[colorIndex]
+				    context.beginPath()
 
 					switch(this.props.shape) {
 						case "square":
-							context.rect(x - (size[i]/2), y - (size[i]/2), size[i], (size[i]/2));
-							break;
+							context.rect(x - (size[i]/2), y - (size[i]/2), size[i], (size[i]/2))
+							break
 
 						default:
-							context.arc(x, y, size[i], 0, 2 * Math.PI, false);
+							context.arc(x, y, size[i], 0, 2 * Math.PI, false)
 					}
 
-				    context.fill();
-					colorIndex ++;
+				    context.fill()
+					colorIndex ++
 
 					if(colorIndex == colors.length) {
-						colorIndex = 0;
+						colorIndex = 0
 					}
 
-					if(this.DEBUGGING) { console.log("rendered at: (" + x + ", " + y + ")"); }
+					if(this.DEBUGGING) { console.log("rendered at: (" + x + ", " + y + ")") }
 				}
 			}	    
 
-			const newSpeed = this.state.speed - (this.RESISTENCE * this.DELTA_TIME / 1000);
-			const newTime = this.state.totalTime + this.DELTA_TIME;
-			const newVerticalSpeed = this.state.verticalSpeed + (500 * this.DELTA_TIME / 1000);
-			const newVerticalDisplacement = this.state.verticalDisplacement + (newVerticalSpeed * this.DELTA_TIME / 1000);
+			const newSpeed = this.state.speed - (this.RESISTENCE * this.DELTA_TIME / 1000)
+			const newTime = this.state.totalTime + this.DELTA_TIME
+			const newVerticalSpeed = this.state.verticalSpeed + (500 * this.DELTA_TIME / 1000)
+			const newVerticalDisplacement = this.state.verticalDisplacement + (newVerticalSpeed * this.DELTA_TIME / 1000)
 
 			this.setState({ 
 				radius : radius, 
@@ -153,23 +187,23 @@ export default class Explosion extends React.Component {
 				totalTime: newTime,
 				verticalSpeed: newVerticalSpeed,
 				verticalDisplacement: newVerticalDisplacement
-			});
+			})
 
 			if(newTime > this.MAX_TIME) {
-				clearInterval(this.state.intervalId);
+				this.safeStopAnimation()
 			}
 		} 
 	}
 
 	render() {
-		const { width, height } = this.state;
+		const { width, height } = this.state
 
 		return (
-		     <canvas ref="canvas" width={width} height={height}
+		     <canvas ref="canvas" width={width} height={height} onClick={this.startAnimation}
 		      style={ { display : `inline`, width : `100%`, height : `100%`} }>
 		     	Your browser doesn't support canvas
 		     </canvas>
-		 );
+		 )
 	}
 
 }
