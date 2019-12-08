33bd0ac8126130244377a9d753416dce289bae16
TuxedoFish <harryliversedge@gmail.com>
2019-12-08 20:47:16 +0000

    Added debugging to polyline

--- src/components/layout/mapping/Polyline.jsx
+++ src/components/layout/mapping/Polyline.jsx
@@ -17,23 +17,57 @@ const wrappedPromise = function() {
 }
 
 class Polyline extends React.Component {
+
+	constructor(props) {
+		super(props)
+
+		this.DEBUGGING = true
+
+		this.arePathsEqual = this.arePathsEqual.bind(this)
+	}
+
 	componentDidMount() {
 	    this.polylinePromise = wrappedPromise();
+
+	    if(this.DEBUGGING) { console.log("componentDidMount: calling render function"); }
+
+	    this.renderPolyline();
+	    this.polyline.setMap(null);
 	    this.renderPolyline();
   	}
 
     componentDidUpdate(prevProps) {
-		if (this.props.map !== prevProps.map) {
+    	const arePathsEqual = this.arePathsEqual(this.props.path, prevProps.path)
+
+    	if(this.DEBUGGING) { console.log("componentDidUpdate: Do i rerender my path? " + !arePathsEqual); }
+
+		if (this.props.map !== prevProps.map || !arePathsEqual) {
 		    if (this.polyline) {
+		    	if(this.DEBUGGING) { console.log("componentDidUpdate: setting map to null...") }
 		    	this.polyline.setMap(null);
 		    }
+		    if(this.DEBUGGING) { console.log("componentDidUpdate: calling render function") }
 		    this.renderPolyline();
 		}
     }
 
+    arePathsEqual(path, prevPath) {
+    	if(path.length != prevPath.length) { return false }
+
+    	for(var i=0; i<path.length; i++) {
+    		if(path[i].lng != prevPath[i].lng || 
+    			path[i].lat != prevPath[i].lat) {
+    			return false
+    		}
+    	}
+
+    	return true
+    }
+
 	componentWillUnmount() {
 	    if (this.polyline) {
-	      this.polyline.setMap(null);
+	    	if(this.DEBUGGING) { console.log("componentWillUnmount: setting map to null...") }
+	    	this.polyline.setMap(null);
 	    }
 	}
 
@@ -49,9 +83,14 @@ class Polyline extends React.Component {
 	        ...props
     	} = this.props;
 
+    	if(this.DEBUGGING) { console.log("Trying to render...") }
+
 	    if (!google) {
+	    	if(this.DEBUGGING) { console.log("Cannot render...") }
 	        return null;
 	    }
+	    
+	    if(this.DEBUGGING) { console.log("Rendering...") }
 
 	    const params = {
 	      map,
